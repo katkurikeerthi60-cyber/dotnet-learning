94c676b20c27ab5f0ce8e608419f5cff4d117f57
Keerthi <katkurikeerthi60@gmail.com>
2026-09-05 17:07:06 +0530

    feat: add methods and tuple exercise

--- HelloDotnet/Program.cs
+++ HelloDotnet/Program.cs
@@ -1,19 +1,25 @@
-﻿for (int i = 1; i <= 30; i++)
+﻿static void Swap(ref int a, ref int b)
 {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine("FizzBuzz");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine("Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine("Buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
-    }
+    int temp = a;
+    a = b;
+    b = temp;
 }
+
+static (int sum, int difference) Calculate(int a, int b)
+{
+    return (a + b, a - b);
+}
+
+int x = 10;
+int y = 20;
+
+Console.WriteLine($"Before swap: x = {x}, y = {y}");
+
+Swap(ref x, ref y);
+
+Console.WriteLine($"After swap: x = {x}, y = {y}");
+
+var (sum, difference) = Calculate(30, 10);
+
+Console.WriteLine($"Sum = {sum}");
+Console.WriteLine($"Difference = {difference}");
